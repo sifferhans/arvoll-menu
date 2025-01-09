12e6bfba76c2efe850fde5d51ea39dad380fe3fc
Sigve Hansen <sighan12@gmail.com>
2025-01-09 23:44:19 +0100

    fix: remove unnecessary comments

--- pb_public/index.js
+++ pb_public/index.js
@@ -1,18 +1,9 @@
 const pb = new PocketBase("http://127.0.0.1:8090");
 const app = document.getElementById("app");
 
-updatePage();
-
-pb.collection("products").subscribe("*", (event) => updatePage());
-pb.collection("categories").subscribe("*", (event) => updatePage());
-
-// get data and put onto page
 function updatePage() {
   pb.collection("categories")
-    .getFullList({
-      // https://pocketbase.io/docs/working-with-relations/#back-relations
-      expand: "products_via_category",
-    })
+    .getFullList({ expand: "products_via_category" })
     .then((categories) => {
       app.innerHTML = "";
 
@@ -28,7 +19,21 @@ function updatePage() {
         container.setAttribute("id", id);
         container.innerHTML = `
           <h2>${name}</h2>
-          ${productList(products)}
+          <ul>
+          ${products
+            .map(
+              (product) =>
+                `<li>
+                  <div>
+                    <span>${product.name}</span>
+                    <hr />
+                    <span>${product.price} kr</span>
+                  </div>
+                  ${product.description && `<p>${product.description}</p>`}
+                </li>`,
+            )
+            .join("")}
+          </ul>
         `;
 
         app.appendChild(container);
@@ -36,20 +41,6 @@ function updatePage() {
     });
 }
 
-function productList(products) {
-  return [
-    "<ul>",
-    ...products.map(
-      (product) =>
-        `<li>
-          <div>
-            <span>${product.name}</span>
-            <hr />
-            <span>${product.price} kr</span>
-          </div>
-          ${product.description && `<p>${product.description}</p>`}
-        </li>`,
-    ),
-    "</ul>",
-  ].join("");
-}
+updatePage();
+pb.collection("products").subscribe("*", () => updatePage());
+pb.collection("categories").subscribe("*", () => updatePage());
