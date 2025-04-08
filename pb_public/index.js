a70789fb7e76f6bb0b40281bb8676b99e6a14c66
Sigve Hansen <sighan12@gmail.com>
2025-04-08 16:14:50 +0200

    fix: remove host

--- pb_public/index.js
+++ pb_public/index.js
@@ -1,6 +1,6 @@
 import PocketBase from "./vendor/pocketbase.js";
 
-const pb = new PocketBase("http://127.0.0.1:8090");
+const pb = new PocketBase(":8090");
 const app = document.getElementById("app");
 
 function updatePage() {
